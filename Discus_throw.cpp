bbd51dc4836a98fc7ff8b478f94683be74e6ce04
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-04-07 19:21:05 +0300

    Add files via upload
    simple condition check

---
+++ Discus_throw.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+   int a, b, c;
+  cin >> a >> b >> c;
+  if (a>=b&&a>=c)
+  {
+    cout << a << endl;
+  }
+  else if (b>=c&&b>=a)
+  {
+    cout << b << endl;
+  }
+  
+  else 
+  {
+    cout << c << endl;
+  }
+  }
+  return 0;
+}
