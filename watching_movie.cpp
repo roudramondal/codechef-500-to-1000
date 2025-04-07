c98f096c5bb72fb3adf6009024141b0ff68b1f09
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-04-07 20:09:47 +0300

    Add files via upload
    simple y divisible by and subtrack from x and then output show

---
+++ watching_movie.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int x, y;
+  cin >> x >> y;
+  int find_value = y / 2;
+  int sub_value = x - find_value;
+  cout << sub_value << endl;
+  return 0;
+}
