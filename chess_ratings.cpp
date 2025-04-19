d04b601361059b01cc9ac80254a88fc9b55cc72d
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-04-19 18:50:43 +0300

    Add files via upload
    here i use ceill funcation and (y-z)/8 that's it

---
+++ chess_ratings.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int x, y;
+    cin >> x >> y;
+    cout << (int)ceill((double)(y - x) / 8) << endl;
+   
+}
+ return 0;
+}
