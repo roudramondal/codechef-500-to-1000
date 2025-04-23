959a049c6df216d8261831995ae2d8be22ec1709
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-04-23 19:19:03 +0300

    Add files via upload
    use abs(a-b) and then (d+k-1)/k

---
+++ Reach_fast.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T; 
+    
+    while (T--) {
+        int A, B, K;
+        cin >> A >> B >> K;
+        
+        int distance = abs(A - B);
+        int steps = (distance + K - 1) / K;
+        
+        cout << steps << endl;
+    }
+    
+    return 0;
+}
